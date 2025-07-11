6b3b09b0a8675e8a77ed48bf8180ae7cef03121f
Germano Schulze Junior <germano_schulze_junior@hotmail.com>
2025-07-11 08:21:10 -0300

    Criando a página clientes

---
+++ src/app/dashboard/customer/new/page.tsx
@@ -0,0 +1,9 @@
+import { Container } from "@/components/container";
+
+export default function NewCustomer() {
+    return (
+        <Container>
+            <h1>Página Cadastro</h1>
+        </Container>
+    )
+}
--- src/app/dashboard/customer/page.tsx
+++ src/app/dashboard/customer/page.tsx
@@ -2,6 +2,8 @@ import { Container } from "@/components/container";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import Link from "next/link";
+import { CardCustomer } from "./components/card";
 
 export default async function Customer() {
   const session = await getServerSession(authOptions);
@@ -12,11 +14,24 @@ export default async function Customer() {
 
   return (
     <Container>
-      <main>
-        <div>
-          <h1>Meus clientes</h1>
+      <main className="mt-9 mb-2">
+        <div className="flex items-center justify-between">
+          <h1 className="text-3xl font-bold">Meus clientes</h1>
+          <Link
+            href="/dashboard/customer/new"
+            className="bg-blue-500 text-white px-4 py-1 rounded"
+          >
+            Novo Cliente
+          </Link>
         </div>
+
+        <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 mt-2">
+          <CardCustomer />
+          <CardCustomer />
+          <CardCustomer />
+        </section>
       </main>
+
     </Container>
   );
 }
