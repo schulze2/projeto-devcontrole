b6549ebaccec09c35ec49be84f9a28d89d1709e8
Germano Schulze Junior <germano_schulze_junior@hotmail.com>
2025-07-13 10:13:19 -0300

    Criando ações nos tickets

--- src/app/dashboard/components/ticket/index.tsx
+++ src/app/dashboard/components/ticket/index.tsx
@@ -1,24 +1,46 @@
+"use client";
+
 import { CustomerProps } from "@/utils/customer.type";
 import { TicketProps } from "@/utils/ticket.type";
-import { FiTrash2, FiFile } from "react-icons/fi";
+import { FiCheckSquare, FiFile } from "react-icons/fi";
+import { api } from "@/lib/api";
+import { useRouter } from "next/navigation";
 
 interface TicketItemProps {
   ticket: TicketProps;
   customer: CustomerProps | null;
 }
 
-export function TicketItem( {customer, ticket}: TicketItemProps) {
+export function TicketItem({ customer, ticket }: TicketItemProps) {
+  const router = useRouter();
+
+  async function handleChangeStatus() {
+    try {
+      const response = await api.patch("/api/ticket", {
+        id: ticket.id,
+      });
+
+      router.refresh();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <>
       <tr className="border-b-2 border-b-slate-200 h-16 last:border-b-0 bg-slate-100 hover:bg-gray-200 duration-300">
         <td className="text-left pl-1">{customer?.name}</td>
-        <td className="text-left hidden sm:table-cell">{ticket.created_at?.toLocaleDateString("pt-BR")}</td>
-        <td className="text-left">
-          <span className="bg-green-500 px-2 py-1 rounded">{ticket.status}</span>
+        <td className="text-left hidden sm:table-cell">
+          {ticket.created_at?.toLocaleDateString("pt-BR")}
         </td>
         <td className="text-left">
-          <button className="mr-2">
-            <FiTrash2 size={24} color="#EF4444" />
+          <span className="bg-green-500 px-2 py-1 rounded">
+            {ticket.status}
+          </span>
+        </td>
+        <td className="text-left">
+          <button className="mr-3" onClick={handleChangeStatus}>
+            <FiCheckSquare size={24} color="#131313" />
           </button>
           <button>
             <FiFile size={24} color="#3b82f6" />
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,7 +5,7 @@ import { redirect } from "next/navigation";
 import Link from "next/link";
 import { TicketItem } from "./components/ticket";
 
-import prismaClient from '@/lib/prisma';
+import prismaClient from "@/lib/prisma";
 
 export default async function Dashboard() {
   const session = await getServerSession(authOptions);
@@ -15,14 +15,14 @@ export default async function Dashboard() {
   }
 
   const tickets = await prismaClient.ticket.findMany({
-    where:{
+    where: {
       userId: session.user.id,
-      status: "ABERTO"
+      status: "ABERTO",
     },
-    include:{
+    include: {
       customer: true,
-    }
-  })
+    },
+  });
 
   return (
     <Container>
@@ -41,20 +41,29 @@ export default async function Dashboard() {
           <thead>
             <tr>
               <th className="font-medium text-left pl-1">CLIENTE</th>
-              <th className="font-medium text-left hidden sm:block">DATA CADASTRO</th>
+              <th className="font-medium text-left hidden sm:block">
+                DATA CADASTRO
+              </th>
               <th className="font-medium text-left">STATUS</th>
               <th className="font-medium text-left">#</th>
             </tr>
           </thead>
           <tbody>
-            {tickets.map(ticket =>(
-              <TicketItem key={ticket.id}
-              customer={ticket.customer}
-              ticket={ticket}
+            {tickets.map((ticket) => (
+              <TicketItem
+                key={ticket.id}
+                customer={ticket.customer}
+                ticket={ticket}
               />
             ))}
           </tbody>
         </table>
+
+        {tickets.length === 0 && (
+          <h1 className="px-2 md:px-0 text-gray-600">
+            Nenhum ticket aberto foi encontrado...
+          </h1>
+        )}
       </main>
     </Container>
   );
