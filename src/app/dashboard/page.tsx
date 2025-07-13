b4df4536bd15b49bdfd621d7a70f450261af00f0
Germano Schulze Junior <germano_schulze_junior@hotmail.com>
2025-07-13 09:39:50 -0300

    Listando todos os tickets

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,12 +5,24 @@ import { redirect } from "next/navigation";
 import Link from "next/link";
 import { TicketItem } from "./components/ticket";
 
+import prismaClient from '@/lib/prisma';
+
 export default async function Dashboard() {
   const session = await getServerSession(authOptions);
 
   if (!session || !session.user) {
     redirect("/");
   }
+
+  const tickets = await prismaClient.ticket.findMany({
+    where:{
+      userId: session.user.id,
+      status: "ABERTO"
+    },
+    include:{
+      customer: true,
+    }
+  })
 
   return (
     <Container>
@@ -35,8 +47,12 @@ export default async function Dashboard() {
             </tr>
           </thead>
           <tbody>
-            <TicketItem />
-            <TicketItem />
+            {tickets.map(ticket =>(
+              <TicketItem key={ticket.id}
+              customer={ticket.customer}
+              ticket={ticket}
+              />
+            ))}
           </tbody>
         </table>
       </main>
