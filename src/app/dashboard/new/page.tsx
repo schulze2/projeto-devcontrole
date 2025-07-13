ea90cf00292fceab1595d99ef11d3d6a553f7eeb
Germano Schulze Junior <germano_schulze_junior@hotmail.com>
2025-07-13 09:16:51 -0300

    conhecendo o server action

--- src/app/dashboard/new/page.tsx
+++ src/app/dashboard/new/page.tsx
@@ -19,6 +19,18 @@ export default async function NewTicket() {
     },
   });
 
+  async function handleRegisterTicket(formData: FormData){
+    "use server"
+
+    const name = formData.get("name")
+    const description = formData.get("description")
+    const customerId = formData.get("customer")
+
+    if(!name || !description || !customerId){
+      return;
+    }
+  }
+
   return (
     <Container>
       <main className="mt-9 mb-2">
@@ -32,13 +44,14 @@ export default async function NewTicket() {
           <h1 className="text-3xl font-bold">Novo Chamado</h1>
         </div>
 
-        <form className="flex flex-col mt-6">
+        <form className="flex flex-col mt-6" action={handleRegisterTicket}>
           <label className="mb-1 font-medium text-lg">Nome do chamado</label>
           <input
             type="text"
             placeholder="Digite o nome do chamado"
             required
             className="w-full border-2 rounded-md px-2 mb-2 h-11"
+            name="name"
           />
           <label className="mb-1 font-medium text-lg">
             Descreva o problema
@@ -47,13 +60,17 @@ export default async function NewTicket() {
             placeholder="descreva o problema..."
             required
             className="w-full border-2 rounded-md px-2 mb-2 h-24 resize-none"
+            name="description"
           ></textarea>
           {customers.length !== 0 && (
             <>
               <label className="mb-1 font-medium text-lg">
                 Selecione o cliente
               </label>
-              <select className="w-full border-2 rounded-md px-2 mb-2 h-11 resize-none bg-white">
+              <select 
+              className="w-full border-2 rounded-md px-2 mb-2 h-11 resize-none bg-white"
+              name="customer"
+              >
                 {customers.map((customer) => (
                   <option key={customer.id} value={customer.id}>
                     {customer.name}
